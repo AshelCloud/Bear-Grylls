f6cb7bcf5fe713135fee7f3a25862d46c99d524a
tkdals684 <41319038+tkdals684@users.noreply.github.com>
2019-07-01 18:56:12 +0900

    2

---
+++ Bear-Grylls/Assets/PSM/ForCopy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForCopy : MonoBehaviour
+{
+    #region Public
+    #endregion
+
+
+    #region SerializeField
+    #endregion
+
+    #region Private
+    #endregion
+
+
+    #region MonoEvents
+    void Awake()
+    {
+    }
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+    #endregion
+
+    #region CallBack
+    #endregion
+}
---
+++ Bear-Grylls/Assets/PSM/RigidbodyController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace PSM
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class RigidbodyController : MonoBehaviour
+    {
+        #region Public
+
+
+        public void AddForce(Vector3 force)
+        {
+            isMove = true;
+            _rigidbody.AddForce(force);
+        }
+        public void SetVelocity(Vector3 velocity)
+        {
+            isMove = true;
+            _rigidbody.velocity = velocity;
+        }
+        public void SetVelocityXZ(Vector3 velocity)
+        {
+            isMove = true;
+            velocity.y = _rigidbody.velocity.y;
+            _rigidbody.velocity = velocity;
+        }
+        public Vector3 GetVelocity()
+        {
+            return _rigidbody.velocity;
+        }
+
+        #endregion
+
+
+        #region SerializeField
+        [SerializeField] Rigidbody _rigidbody;
+
+        #endregion
+
+        #region Private
+        private bool controllerStop = true;
+        private float _stopPower = 1;
+
+        private bool isMove = false;
+
+
+        private void ControllerStop()
+        {
+            if (_useControllerStop)
+            {
+                if (isMove == false)
+                {
+                    Vector3 stopVector = Vector3.MoveTowards(_rigidbody.velocity, Vector3.zero, _stopPower * Time.deltaTime);
+                    stopVector.y = _rigidbody.velocity.y;
+
+                    _rigidbody.velocity = stopVector;
+                }
+                isMove = false;
+            }
+        }
+        #endregion
+
+
+        #region MonoEvents
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        private void FixedUpdate()
+        {
+            ControllerStop();
+        }
+        #endregion
+    }
+
+}
+
