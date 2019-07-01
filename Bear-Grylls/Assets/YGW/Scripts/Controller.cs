9f6dd92f87d9e1281673ee9ca03af0f10c9ddb2a
AubeCiel <kk01045422490@gmail.com>
2019-07-01 17:59:19 +0900

    2919.06.30

---
+++ Bear-Grylls/Assets/YGW/Scripts/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = transform.forward * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = -transform.forward * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -transform.right * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * speed * Time.deltaTime;
+        }
+    }
+}
