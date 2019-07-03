b8fac73f73ab9a018e69e15d03ce2493b66d0fad
tkdals684 <41319038+tkdals684@users.noreply.github.com>
2019-07-03 23:15:20 +0900

    아이템 인벤토리 채집 작업

---
+++ Bear-Grylls/Assets/PSM/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSM
+{
+    public abstract class Item : MonoBehaviour
+    {
+        #region Variable
+        public new string name { get; private set; }
+        public string information { get; private set; }
+        public int weight { get; private set; }
+        #endregion
+
+
+        #region Function
+        /// <summary>
+        /// name, information, weight등의 값을 초기화해주세요 Init을 사용하지 않아도 됩니다
+        /// </summary>
+        public abstract void Init();
+        public abstract void Use();
+        #endregion
+    }
+}
