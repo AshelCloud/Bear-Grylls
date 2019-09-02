b01ea3cde854198318eac5a7fdbc8322b0c56ca3
AubeCiel <kk01045422490@gmail.com>
2019-09-02 19:35:10 +0900

    Merge remote-tracking branch 'origin/PSM' into YGW

---
+++ Bear-Grylls/Assets/PSM/Character Script/AnimatorManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSM
+{
+    /// <summary>
+    /// AnimatorManager 컴포넌트가 있어야 작동합니다
+    /// [RequireComponent(typeof(AnimatorManager))]  를 활용하세요
+    /// </summary>
+    public interface UsingAnimatorManagerComponent
+    {
+        /// <summary>
+        /// AnimatorManager 컴포넌트에 의해 Start단계에서 한번만 호출 됩니다
+        /// </summary>
+        /// <param name="animator">AnimatorManager 컴포넌트에 할당된 animator 입니다</param>
+        void ForInit(Animator animator, AnimatorManager aniManager);
+
+        /// <summary>
+        /// AnimatorManager 컴포넌트에 의해 Update단계에 매번 호출됩니다
+        /// </summary>
+        /// <param name="animator">AnimatorManager 컴포넌트에 할당된 animator 입니다</param>
+        void UpdateAnimator(Animator animator, AnimatorManager aniManager);
+    }
+
+
+    public class AnimatorManager : MonoBehaviour
+    {
+        #region Variable - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        [SerializeField] private bool editComponentField = false;
+        [ConditionalHide("editComponentField", true)]
+        [SerializeField] private Animator animator;
+
+        [SerializeField] RuntimeAnimatorController[] controller;
+
+        private UsingAnimatorManagerComponent[] usingAnimatorComponents;
+        #endregion - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        #region Function - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        public void ChangeController(string name)
+        {
+            RuntimeAnimatorController FindedContrl = null;
+            for (int i = 0; i < controller.Length; ++i)
+            {
+                if (controller[i].name == name)
+                {
+                    FindedContrl = controller[i];
+                    break;
+                }
+            }
+            if (FindedContrl == null)
+                return;
+
+            animator.runtimeAnimatorController = FindedContrl;
+        }
+        #endregion - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        #region MonoEvents - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        private void Awake()
+        {
+            if (animator == null) animator = GetComponent<Animator>();
+
+            usingAnimatorComponents = GetComponents<UsingAnimatorManagerComponent>();
+        }
+        private void Start()
+        {
+            for (int i = 0; i < usingAnimatorComponents.Length; ++i)
+                usingAnimatorComponents[i].ForInit(animator, this);
+        }
+        void Update()
+        {
+            for (int i = 0; i < usingAnimatorComponents.Length; ++i)
+                usingAnimatorComponents[i].UpdateAnimator(animator, this);
+        }
+        #endregion - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    }
+}
+
+
---
+++ Bear-Grylls/Assets/PSM/Collections Script/NaturalCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSM
+{
+    public class NaturalCollection : CollectableObject, HavingCollectableActionComponent
+    {
+        #region Variable - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        [SerializeField] float reBuildTime = 10;
+
+        private Renderer renderer;
+        private Collider collider;
+        private float reBuildTimer;
+        #endregion - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        #region Function - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        public void ActionCollected()
+        {
+            renderer.enabled = false;
+            collider.enabled = false;
+            reBuildTimer = reBuildTime;
+        }
+        private void Collected(){}
+        #endregion - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        #region Coroutine - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        private IEnumerator CollectedRoutine(){yield return null;}
+        #endregion - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        #region MonoEvents - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        protected virtual void Awake()
+        {
+            base.Awake();
+            renderer = GetComponent<Renderer>();
+            collider = GetComponent<Collider>();
+
+            reBuildTimer = 0;
+        }
+        protected virtual void Update()
+        {
+            if (0 < reBuildTimer)
+                reBuildTimer -= Time.deltaTime;
+
+            if(reBuildTimer < 0)
+            {
+                renderer.enabled = true;
+                collider.enabled = true;
+            }
+        }
+        #endregion - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    }
+
+}
